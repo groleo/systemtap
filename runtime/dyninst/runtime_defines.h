4fe296d99991320c7f6da2fd516e78dbe19f4cb6
Josh Stone <jistone@redhat.com>
2012-10-03 12:59:15 -0700

    stapdyn: nullify the pagefault machinations for derefs
    We don't need to care about pagefault safety in userspace, but the
    definitions making those into preempt_disable led to recursing on
    stapdyn_big_dumb_lock (going away in PR14571).  We can just #define
    the pagefault_enable/disable away for the dyninst runtime.

--- runtime/dyninst/runtime_defines.h
+++ runtime/dyninst/runtime_defines.h
@@ -1,1 +1,6 @@
 #define STP_NO_OVERLOAD 1
+
+/* We don't need to worry about pagefaults in loc2c-runtime.h */
+#define STAPCONF_PAGEFAULT_DISABLE  1
+#define pagefault_disable()
+#define pagefault_enable()
