5d03e8dd7798b0c29d9c5192920b31b2901e88cc
Josh Stone <jistone@redhat.com>
2012-11-21 12:26:51 -0800

    stapdyn: Add an offptr debug mode for direct pointers
    Useful for validation, and also to compare how the offset computations
    affect code generation.

--- runtime/dyninst/offptr.h
+++ runtime/dyninst/offptr.h
@@ -14,6 +14,10 @@
 /* An offset pointer refers to memory without using an absolute address.  This
  * is useful for shared memory between processes, and perhaps also for cases
  * where memory may move, as with realloc.  */
+
+
+#ifndef OFFPTR_DEBUG_DIRECT
+
 typedef struct {
 	/* The offset is always relative to the offptr_t itself.  */
 	ptrdiff_t offset;
@@ -30,6 +34,17 @@ offptr_set(offptr_t* op, void* ptr)
 {
 	op->offset = ptr - (void*)op;
 }
+
+#else
+/* OFFPTR_DEBUG_DIRECT: In this mode, offptr_t is basically a plain pointer
+ * again.  Despite the level of abstraction, it should compile down to the same
+ * as using direct pointers would.
+ */
+typedef struct { void* pointer; } offptr_t;
+static inline void * offptr_get(offptr_t* op) { return op->pointer; }
+static inline void offptr_set(offptr_t* op, void* ptr) { op->pointer = ptr; }
+#endif
+
 
 /* Since offptr_t is untyped, this template-like macro lets you define
  * accessors with the appropriate pointer types enforced.  */
