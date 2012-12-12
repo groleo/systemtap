groleo/systemtap

commit 065d55678ff39ec41d2a8ec8b6e0a7916eef5ba6

stapdyn: Move global session counters into shm
In dyninst mode, the atomic_t session_state, error_count, and various
skipped_counts are now lumped into a common structure which is allocated
first thing in shared memory.  Kernel mode is effectively unchanged.

* runtime/common_session_state.h: Split into runtime specifics.
* runtime/dyninst/common_session_state.h: Define a session struct,
  access functions, and an initialization function using shm.
* runtime/linux/common_session_state.h: Leave the former atomics as
  globals, but provide the same function abstractions as in /dyninst/.
* translate.cxx (c_unparser::emit_module_init): Call stp_session_init.
* [everything else]: Update how those global variables are accessed,
  essentially just s/&foo/foo()/g.

runtime/linux/uprobes-common.c
@@ -119,8 +119,8 @@ static int stap_uprobe_change_plus (struct task_struct *tsk, unsigned long reloc
       #endif
       /* NB: duplicates common_entryfn_epilogue,
 	 but then this is not a probe entry fn epilogue. */
-      if (unlikely (atomic_inc_return (& skipped_count) > MAXSKIPPED)) {
-        if (unlikely (pseudo_atomic_cmpxchg(& session_state, STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
+      if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
+        if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
         _stp_error ("Skipped too many probes, check MAXSKIPPED or try again with stap -t for more details.");
       }
     }

runtime/common_session_state.h
@@ -5,11 +5,13 @@
 #define STAP_SESSION_ERROR 2
 #define STAP_SESSION_STOPPING 3
 #define STAP_SESSION_STOPPED 4
-static atomic_t session_state = ATOMIC_INIT (STAP_SESSION_STARTING);
 
-static atomic_t error_count = ATOMIC_INIT (0);
-static atomic_t skipped_count = ATOMIC_INIT (0);
-static atomic_t skipped_count_lowstack = ATOMIC_INIT (0);
-static atomic_t skipped_count_reentrant = ATOMIC_INIT (0);
-static atomic_t skipped_count_uprobe_reg = ATOMIC_INIT (0);
-static atomic_t skipped_count_uprobe_unreg = ATOMIC_INIT (0);
+#if defined(__KERNEL__)
+
+#include "linux/common_session_state.h"
+
+#elif defined(__DYNINST__)
+
+#include "dyninst/common_session_state.h"
+
+#endif

runtime/dyninst/common_session_state.h
@@ -1,0 +1,70 @@
+// Will be included once by runtime/common_session_state.h, which is included
+// once by translate.cxx c_unparser::emit_common_header ().
+
+
+// Forward declarations for things in runtime/dyninst/shm.c
+static void *_stp_shm_base;
+static void *_stp_shm_alloc(size_t size);
+
+
+// Global state shared throughout the module
+struct stp_runtime_session {
+	atomic_t _session_state;
+
+	atomic_t _error_count;
+	atomic_t _skipped_count;
+	atomic_t _skipped_count_lowstack;
+	atomic_t _skipped_count_reentrant;
+	atomic_t _skipped_count_uprobe_reg;
+	atomic_t _skipped_count_uprobe_unreg;
+};
+
+static inline struct stp_runtime_session* _stp_session(void)
+{
+	// Since the session is always the first thing allocated, it lives
+	// directly at the start of shared memory.
+	// NB: We always reference the main shm pointer, rather than our own
+	// local copy, because reallocations may cause it to move dynamically.
+	return _stp_shm_base;
+}
+
+
+#define GET_SESSION_ATOMIC(name)			\
+	static inline atomic_t *name(void) {		\
+		return &_stp_session()->_##name;	\
+	}
+
+GET_SESSION_ATOMIC(session_state);
+
+GET_SESSION_ATOMIC(error_count);
+GET_SESSION_ATOMIC(skipped_count);
+GET_SESSION_ATOMIC(skipped_count_lowstack);
+GET_SESSION_ATOMIC(skipped_count_reentrant);
+GET_SESSION_ATOMIC(skipped_count_uprobe_reg);
+GET_SESSION_ATOMIC(skipped_count_uprobe_unreg);
+
+#undef GET_SESSION_ATOMIC
+
+
+static int stp_session_init(void)
+{
+	// Reserve space for the session at the beginning of shared memory.
+	void *session = _stp_shm_zalloc(sizeof(struct stp_runtime_session));
+	if (!session)
+		return -ENOMEM;
+
+	// If we weren't the very first thing allocated, then something is wrong!
+	if (session != _stp_shm_base)
+		return -EINVAL;
+
+	atomic_set(session_state(), STAP_SESSION_STARTING);
+
+	atomic_set(error_count(), 0);
+	atomic_set(skipped_count(), 0);
+	atomic_set(skipped_count_lowstack(), 0);
+	atomic_set(skipped_count_reentrant(), 0);
+	atomic_set(skipped_count_uprobe_reg(), 0);
+	atomic_set(skipped_count_uprobe_unreg(), 0);
+
+	return 0;
+}

runtime/dyninst/probe_lock.h
@@ -59,7 +59,7 @@ stp_lock_probe(const struct stp_probe_lock *locks, unsigned num_locks)
 	return 1;
 
 skip:
-	atomic_inc(&skipped_count);
+	atomic_inc(skipped_count());
 #ifdef STP_TIMING
 	atomic_inc(locks[i].skipped);
 #endif

runtime/linux/common_session_state.h
@@ -1,0 +1,27 @@
+// Will be included once by runtime/common_session_state.h, which is included
+// once by translate.cxx c_unparser::emit_common_header ().
+
+#define DEFINE_SESSION_ATOMIC(name, init)		\
+	static atomic_t g_##name = ATOMIC_INIT (init);	\
+	static inline atomic_t *name(void) {		\
+		return &g_##name;			\
+	}
+
+DEFINE_SESSION_ATOMIC(session_state, STAP_SESSION_STARTING);
+
+DEFINE_SESSION_ATOMIC(error_count, 0);
+DEFINE_SESSION_ATOMIC(skipped_count, 0);
+DEFINE_SESSION_ATOMIC(skipped_count_lowstack, 0);
+DEFINE_SESSION_ATOMIC(skipped_count_reentrant, 0);
+DEFINE_SESSION_ATOMIC(skipped_count_uprobe_reg, 0);
+DEFINE_SESSION_ATOMIC(skipped_count_uprobe_unreg, 0);
+
+#undef DEFINE_SESSION_ATOMIC
+
+
+static int stp_session_init(void)
+{
+	// All the above atomics have static initialization.
+	// Nothing else to do...
+	return 0;
+}

runtime/linux/probe_lock.h
@@ -60,7 +60,7 @@ stp_lock_probe(const struct stp_probe_lock *locks, unsigned num_locks)
 	return 1;
 
 skip:
-	atomic_inc(&skipped_count);
+	atomic_inc(skipped_count());
 #ifdef STP_TIMING
 	atomic_inc(locks[i].skipped);
 #endif

runtime/time.c
@@ -133,7 +133,7 @@ __stp_time_timer_callback(unsigned long val)
        XXX: The worst that can probably happen is that we get
 	    two consecutive timer resets.  */
 
-    if (likely(atomic_read(&session_state) != STAP_SESSION_STOPPED))
+    if (likely(atomic_read(session_state()) != STAP_SESSION_STOPPED))
         mod_timer(&time->timer, jiffies + 1);
 }
 

tapset-procfs.cxx
@@ -258,8 +258,8 @@ procfs_derived_probe_group::emit_module_decls (systemtap_session& s)
       s.op->newline(1) << "c->ips.procfs_data = &pdata;";
       s.op->newline(-1) << "else {";
 
-      s.op->newline(1) << "if (unlikely (atomic_inc_return (& skipped_count) > MAXSKIPPED)) {";
-      s.op->newline(1) << "atomic_set (& session_state, STAP_SESSION_ERROR);";
+      s.op->newline(1) << "if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {";
+      s.op->newline(1) << "atomic_set (session_state(), STAP_SESSION_ERROR);";
       s.op->newline() << "_stp_exit ();";
       s.op->newline(-1) << "}";
       s.op->newline() << "atomic_dec (& c->busy);";
@@ -316,8 +316,8 @@ procfs_derived_probe_group::emit_module_decls (systemtap_session& s)
       s.op->newline(1) << "c->ips.procfs_data = &pdata;";
       s.op->newline(-1) << "else {";
 
-      s.op->newline(1) << "if (unlikely (atomic_inc_return (& skipped_count) > MAXSKIPPED)) {";
-      s.op->newline(1) << "atomic_set (& session_state, STAP_SESSION_ERROR);";
+      s.op->newline(1) << "if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {";
+      s.op->newline(1) << "atomic_set (session_state(), STAP_SESSION_ERROR);";
       s.op->newline() << "_stp_exit ();";
       s.op->newline(-1) << "}";
       s.op->newline() << "atomic_dec (& c->busy);";

commit 4e1434f61e425f55cabfcf955457d7c7c53884bb

Fix a couple more missed atomic changes
More fallout from commit 065d5567...

runtime/linux/uprobes-common.c
@@ -115,7 +115,7 @@ static int stap_uprobe_change_plus (struct task_struct *tsk, unsigned long reloc
     /* NB: handled_p implies slotted_p */
     if (unlikely (! handled_p)) {
       #ifdef STP_TIMING
-      atomic_inc (& skipped_count_uprobe_reg);
+      atomic_inc (skipped_count_uprobe_reg());
       #endif
       /* NB: duplicates common_entryfn_epilogue,
 	 but then this is not a probe entry fn epilogue. */

testsuite/systemtap.base/optim_arridx.exp
@@ -11,7 +11,7 @@ elide_global_b:long
 # functions
 exit:unknown ()
 %{ /* unprivileged */
-    atomic_set (&session_state, STAP_SESSION_STOPPING);
+    atomic_set (session_state(), STAP_SESSION_STOPPING);
     _stp_exit ();
 %}
 fna:long (a:long)