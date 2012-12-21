19099da1c6b3e20a1ef4289cea9bdc8325758886
Frank Ch. Eigler <fche@redhat.com>
2012-12-21 12:55:53 -0500

    CHECK_ERRWARN_MANPAGES.sh: stop trying to return a rc=1 from KO
    (Since rc=1 from the while loop doesn't make it out of the loop.
    /me hugs forked subshells.)

--- man/CHECK_ERRWARN_MANPAGES.sh
+++ man/CHECK_ERRWARN_MANPAGES.sh
@@ -2,9 +2,7 @@
 
 # Run this in source tree, to check whether all error/warning man pages mentioned
 # in the systemtap sources actually exist.
-
 set -e
-rc=0
 
 # NB: we don't want this regexp to match itself
 (cd ..; git grep -E '\[man (error|warning)::.*\]' | cut -f2 -d[ | cut -f1 -d] | cut -f2 -d' ') | while read manpage
@@ -14,8 +12,5 @@ do
         echo "ok $file"
     else
         echo "KO $file"
-	rc=1
     fi
 done
-
-exit $rc
